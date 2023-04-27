681ded1311d5311f5ba2d5716265af62a4a439a4
pavel <KarasevPavel>
2023-04-27 09:24:36 +0300

    [MPCC-5] (-) all unnecessary old files were deleted

--- Matlab/getNormalizationParameters.m
+++
@@ -1,6 +0,0 @@
-function normalization_parameters = getNormalizationParameters()
-%GETNORMALIZATION Summary of this function goes here
-%   Detailed explanation goes here
-NormalizationParameters
-end
-
